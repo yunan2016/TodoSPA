0ffd7ec55f64d5404b6483357d4a674e2033e8ae
v-nany <v-nany@MININT-J38VBUG>
2017-06-09 14:40:46 +0800

    init commit

---
+++ TodoSPA/App/Scripts/app.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+        requireADLogin: true,
+    }).otherwise({ redirectTo: "/Home" });
+
+    var endpoints = {
+        "https://graph.windows.net/": "https://graph.windows.net/",
+    };
+
+
+    adalProvider.init(
+        {
+                instance: 'https://login.microsoftonline.com/', 
+            tenant: 'testbasic1.onmicrosoft.com',
+            clientId: '4bcce6e0-1d19-4235-93be-9026a3f0c2e3',
+            extraQueryParameter: 'nux=1',
+            endpoints: endpoints,
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+   
+}]);
---
+++ TodoSPA/App/Scripts/userDataCtrl.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+angular.module('todoApp')
+.controller('userDataCtrl', ['$scope', 'adalAuthenticationService','$http' ,function ($scope, adalService, $http, config) {
+    $scope.message = 'Fetching data from the API:';
+    //$scope.userInfo = adalAuthenticationService.userInfo;
+
+    //$http.get('https://graph.windows.net/78033c5f-2680-48c1-a4c3-03756ea8d498/reports?api-version=beta').success(function (results) {
+
+    //    var a = '';
+    //}).error(function (results) {
+
+    //    var b = ''
+    //})
+
+
+    var client = $http({
+        method: 'GET',
+        url: "https://graph.windows.net/testbasic1.onmicrosoft.com/me?api-version=1.6",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+
+    });
+
+    client.then(function (data) {
+        console.log(data);
+        $scope.message = "Data is fetched from the API with Access Token";
+
+        $scope.apiData = data.data;
+    }, function (data) {
+        console.log(data)
+        $scope.message = data.data;
+    });
+
+}]);
