83fd9596c61252ec2593dbf33a5e0cc805f87886
yunan2016 <v-nany@hotmail.com>
2017-06-09 14:45:22 +0800

    Create userDataCtrl.js

--- TodoSPA/App/Scripts/userDataCtrl.js
+++ TodoSPA/App/Scripts/userDataCtrl.js
@@ -2,17 +2,6 @@
 angular.module('todoApp')
 .controller('userDataCtrl', ['$scope', 'adalAuthenticationService','$http' ,function ($scope, adalService, $http, config) {
     $scope.message = 'Fetching data from the API:';
-    //$scope.userInfo = adalAuthenticationService.userInfo;
-
-    //$http.get('https://graph.windows.net/78033c5f-2680-48c1-a4c3-03756ea8d498/reports?api-version=beta').success(function (results) {
-
-    //    var a = '';
-    //}).error(function (results) {
-
-    //    var b = ''
-    //})
-
-
     var client = $http({
         method: 'GET',
         url: "https://graph.windows.net/testbasic1.onmicrosoft.com/me?api-version=1.6",
